9217c1f00c11d350784de16be531efdc7f6b9e76
Marcia <tejedamarcia@gmail.com>
2017-04-05 01:18:39 -0300

    775-optimizar!

--- 775.cpp
+++ 775.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 int n = 0;
+bool encontre = false;
 
 vector<pair<int, int> > conecta_con(int inicio, vector<pair<int,int> > r){
     vector<pair<int, int> > rutas;
@@ -47,35 +48,34 @@ bool existe_nodo(int nodo, vector<pair<int, int> > rutas){
     return false;
 }
 
-bool paso_x_todos(vector<pair<int,int> > ca){
+bool es_hamiltoniano(vector<pair<int,int> > ca){
     for (int i = 1; i<=n; i++){
         if (!existe_nodo(i, ca)) return false;
     }
     return true;
 }
 
-bool es_hamiltoniano(vector<pair<int, int> > rutas){
-    return paso_x_todos(rutas);
-}
+
 
 
 string armar_result(vector <pair<int,int>> camino){
-  string resultado = "";
-  for (int i =0; i<camino.size(); i++){
-    std::string r = std::to_string(camino[i].first);
-    resultado = resultado + r + " ";
-    if (i+1 == camino.size()){
-      std::string r = std::to_string(camino[i].second);
-      resultado = resultado + r;
+    string resultado = "";
+    for (int i =0; i<camino.size(); i++){
+        std::string r = std::to_string(camino[i].first);
+        resultado = resultado + r + " ";
+        if (i+1 == camino.size()){
+            std::string r = std::to_string(camino[i].second);
+            resultado = resultado + r;
+        }
     }
-  }
-return resultado;
+    return resultado;
 }
 
 //en inicial veo desde dónde salgo para ver cuando llego al final
 void ciclo(int inicial, int inicio, vector<pair<int,int> > rutas, vector<pair<int,int> > &ca){
     vector<pair<int,int> >  rutas_conectadas = conecta_con(inicio,  rutas);
     //si en el camino actual tengo la misma cant de rutas que el total de rutas, llegué al final
+    if (encontre) return;
     for(int i= 0; i<rutas_conectadas.size(); i++){
         if (!existe_ruta(rutas_conectadas[i], ca) && !existe_nodo(rutas_conectadas[i].second, ca)){// ELSE si el nodo existe--> ver si llegue al final!
             ca.push_back(rutas_conectadas[i]);
@@ -86,44 +86,33 @@ void ciclo(int inicial, int inicio, vector<pair<int,int> > rutas, vector<pair<in
             if (rutas_conectadas[i].second == inicial){
                 ca.push_back(rutas_conectadas[i]);
                 if (es_hamiltoniano(ca)){
-                  string resultado = armar_result(ca);
-                  std::cout << resultado << '\n';
+                    string resultado = armar_result(ca);
+                    std::cout << resultado << '\n';
+                    encontre = true;
                 }
                 ca.pop_back();
             }
         }
     }
-    //std::cout << "N" << '\n';
     return;
 }
-/*
-bool hay_camino(vector <pair<int, int> >  rutas){
-  std::cout << "haycamino" << '\n';
-  vector <pair<int, int> > camino;
-  for (int i = 1; i<=n; i++){
-    std::cout << "entro al for" << '\n';
-      vector<pair<int,int> > camino_actual;
-      camino = ciclo(i, i, rutas, camino_actual);
-      for (int j = 0; j<camino.size(); j++){
-        std::cout << camino[i].first << " " << camino[i].second << '\n';
-      }
-      if (camino.size()>0) return true;
-  }
-  return false;
-}*/
 
 
 int main(){
-    vector <pair<int, int> > rutas;
+
     while (cin >> n){
+        vector <pair<int, int> > rutas;
+        encontre = false;
         string a, b;
-        while(cin >> a) {
+        while(!encontre && cin >> a)  {
             if (a == "%"){
-              //if (!hay_camino(rutas)) std::cout << "N" << '\n';
-              for (int i = 1; i<=n; i++){
-                vector <pair<int, int> > camino_actual;
-                ciclo(i, i, rutas, camino_actual);
-              }
+                for (int i = 1; i<=n; i++){
+                    vector <pair<int, int> > camino_actual;
+                    ciclo(i, i, rutas, camino_actual);
+                    if (encontre) i = n+1;
+                }
+                if (!encontre) {std::cout << "N" << '\n';
+                                    break;};
             }
             else {
                 int b;
@@ -131,8 +120,9 @@ int main(){
                 int c =   std::atoi( a.c_str() );
                 rutas.push_back(make_pair(c, b));
             }
+
         }
-        n = 0;
+        //n = 0;
     }
 
 }
