9015bca2c2288486cf6bcd208cc555f0795f21f8
Marcia <tejedamarcia@gmail.com>
2017-04-06 18:00:59 -0300

    775

---
+++ pruebas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include<cstdio>
+#include<cstring>
+#include<map>
+#include<vector>
+#include <string>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+  for (int i = 0; i < 30; i++){
+    std::cout << i << '\n';
+    i = 30;
+
+  }
+
+}
