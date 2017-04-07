d6b49d23ce690c72a0f669c9112e5d3e7c973d29
Marcia Tejeda <tejedamarcia@gmail.com>
2017-04-07 09:45:23 -0300

    p

---
+++ pruebas.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <array>
+
+
+using namespace std;
+
+int n = 7;
+
+bool existe_nodo(int nodo, vector<pair<int, int>> rutas){
+  for (int i = 0; i<rutas.size(); i++){
+    if (rutas[i].first == nodo || rutas[i].second == nodo){
+      return true;
+    }
+  }
+  return false;
+}
+
+bool paso_x_todos(vector<pair<int,int>> ca){
+  for (int i = 1; i<=n; i++){
+    if (!existe_nodo(n, ca)) return false;
+  }
+  return true;
+}
+
+
+int main()
+{
+
+bool v[10];
+v[2] = 1;
+
+for (int i = 0; i<10; i++){
+  if (v[i]) std::cout << v[i] << '\n';
+}
+
+  /*
+vector<pair<int,int>> rutas;
+rutas.push_back(make_pair(1,2));
+rutas.push_back(make_pair(2,3));
+rutas.push_back(make_pair(2,4));
+rutas.push_back(make_pair(3,4));
+rutas.push_back(make_pair(3,1));
+
+bool p = paso_x_todos(rutas);
+
+std::cout << p << '\n';
+*/
+/*
+vector <int> j;
+j.resize(8);
+j[2] = 1;
+
+for (int i = 0; i<8; i++){
+  std::cout << j[i] << '\n';
+}*/
+
+/*imprimir una matriz
+
+for (int i=0;i<10;i++)
+      {
+        for (int j=0;j<10;j++)
+        {
+          std::cout << "|" << i << j << "  "  <<matrizRutas[i][j] << "|";
+
+        }
+        std::cout << '\n';
+
+      }
+
+*/
+
+}
