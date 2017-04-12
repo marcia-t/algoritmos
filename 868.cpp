e28074e98ae37e2781757cba950ac8c910a3ad79
Marcia <tejedamarcia@gmail.com>
2017-04-12 00:16:54 -0300

    maze

--- 868.cpp
+++ 868.cpp
@@ -7,23 +7,48 @@
 #include<cstdlib>
 
 using namespace std;
-int cases = 0;
-constexpr int filas;
-constexpr int cols;
+int CASES;
+int FILAS;
+int COLS;
+int COLUMNA_SALIDA;
+bool ENCONTRE =false;
+int maze[1000][1000];
+bool visitados[1000][1000];
 
-int maze[filas][cols];
-bool visitados[filas][cols];
+
+void   go(/*int maze[FILAS+1][COLS+1], bool visitados[FILAS+1][COLS+1],*/int fila, int columna, int proximo, int limite){
+  if (maze[fila][columna] != proximo || visitados[fila][columna] || fila<1 || columna <1 || fila > FILAS || columna > COLS){
+    return;
+  }
+  if (fila == FILAS){
+    ENCONTRE = true;
+    COLUMNA_SALIDA = columna;
+    return;
+  }
+  proximo++;
+  if(proximo > limite) {
+    proximo = 1;
+    limite++;
+  }
+  visitados[fila][columna] = true;
+    go(/*maze, visitados,*/ fila, columna-1, proximo, limite);
+    go(/*maze, visitados,*/ fila+1, columna, proximo, limite);
+    go(/*maze, visitados,*/ fila, columna+1, proximo, limite);
+
+}
 
 
 int main(){
-  while (cin >> cases){
-    cin >> filas;
-    cin >> cols;
+  while (cin >> CASES){
+    cin >> FILAS;
+    cin >> COLS;
 
-    int maze[filas][cols];
+    /*int maze[FILAS+1][COLS+1];
+    bool visitados[FILAS+1][COLS+1];*/
 
-    for (int i = 0; i < filas; i++){
-      for (int j = 0; j < cols; j++){
+
+    for (int i = 1; i <= FILAS; i++){
+      for (int j = 1; j <= COLS; j++){
         int a;
         cin >> a;
         maze[i][j] = a;
@@ -31,8 +56,13 @@ int main(){
       }
     }
 
-
-
+    for (int i = 1; i<=COLS; i++){
+      go(/*maze, visitados, */1, i,1, 1);
+      if (ENCONTRE){
+        std::cout << COLUMNA_SALIDA << '\n';
+        break;
+      }
+    }
 
 
   }
