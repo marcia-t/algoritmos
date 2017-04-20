7de631b07cf258f1e6b3128bcf1bac659cdbc0d8
Marcia <tejedamarcia@gmail.com>
2017-04-20 00:28:16 -0300

    11517

--- 11517.cpp
+++ 11517.cpp
@@ -7,27 +7,38 @@
 #include<cstdlib>
 
 using namespace std;
+using pp = pair<int,int>;
+using ll = long long;
 
 
-constexpr int CASES = 0;
+int CASES = 0;
 constexpr int MAX_PRICE = 10000;
 constexpr int MAX_COINS = 100;
 vector<int> coins;
+vector<vector<pp>> mem;
+constexpr ll INF = (100*100+1);
 
+
+ll minp (int ptp, int c){
+  
+}
 
 int main (){
   cin >> CASES;
   for (int i = 0; i< CASES; i++){
-    int ptp, int c;
+
+    mem.assign(MAX_PRICE+1, vector<pp>(MAX_COINS+1, make_pair(-INF, -INF)));
+
+    int ptp, c;
     cin >> ptp;
     cin>> c;
-    coins.resize(c);
-    for (int i = 0; i < c; i++){
+    coins.resize(c+1);
+    for (int i = 1; i <= c; i++){
       int a;
       cin >>a;
       coins[i] = a;
     }
-
+    minp(ptp, c);
   }
 }
 /*
