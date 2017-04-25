eef51c4aa26049cac72509da52b915e763f0336d
Marcia <tejedamarcia@gmail.com>
2017-04-25 01:24:09 -0300

    fuckyou 11517

--- 11003.cpp
+++ 11003.cpp
@@ -16,19 +16,16 @@ constexpr int INV = -1;
 int mem[MAX_BOXES+1][MAX_WEIGHT+1];
 int weights[MAX_WEIGHT];
 int loads[MAX_LOAD];
-//GUARDO LA CANTIDAD DE CAJAS QUE VOY  A TENER QUE RECORRER
+//Guardo la cntidad de cajas que tengo y que voy a tener q recorrer
 int a_boxes;
-//GUARDO LA CARGA MÁS GRANDE QUE ENCUENTRO (PARA USAR MENOS MEMORIA EN MI ESTRUCTURA DE MEMOIZACION Y
-//NO INICIALIZAR TODOS LOS VAlores hasta max_load)
-int h_load=0;
 
 
-//load representa el resto de capacidad que me queda para llenar.
+//load representa el resto de capacidad que me queda para llenar
 int max_h(int box, int load){
     //si ya recorrí todas las cajas restantes.
     if (box == a_boxes) return 0;
     if (mem[box][load] == INV){
-      //calculo el resultado de no usar la caja actual
+      //calculo el resultado de no usar la caja actual (paso a la siguiente caja con el mismo resto de carga disponible)
         int not_using =  max_h(box + 1, load);
         mem[box][load] = not_using;
         if (load >= weights[box]) //si todavía me queda lugar para apilar la que viene, hago la comparación entre usar y no usar la caja actual
@@ -48,16 +45,14 @@ int main(){
             cin >> l;
             weights[i] = w;
             loads[i] = l;
-            h_load = max(h_load, l);
         }
-
         for (int i = 0; i <= a_boxes; ++i)
-            for (int j = 0; j <= h_load; ++j)
+            for (int j = 0; j <= MAX_LOAD; ++j)
                 mem[i][j] = INV;
-
         int highest = 0;
+        
         for (int h=0; h<a_boxes;h++){
-            //arranco USANDO la caja actual
+          //uso la primer caja (todas pueden ser la primera)
             highest = max(highest, max_h(h+1, loads[h])+1);
         }
         std::cout << highest << '\n';
