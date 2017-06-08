20e91f0fc12547ae95e095e2ad39a2714a5354b0
Marcia <tejedamarcia@gmail.com>
2017-06-08 00:41:35 -0300

    init 567

---
+++ entrega8/567.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include<algorithm>
+#include<string>
+#include<list>
+
+using namespace std;
+using vi = vector<int>;
+using graph = vector<vi>;
+constexpr int UNDEFINED = -1;
+
+
+
+int main(){
+  graph distance;
+  distance.assign(20, vi());
+  int a;
+
+}
