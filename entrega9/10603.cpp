072a624b6d051e79db2596bf35f683e1e63cb399
Marcia <tejedamarcia@gmail.com>
2017-06-14 23:58:41 -0300

    10603

---
+++ entrega9/10603.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <list>
+#include <map>
+#include <sstream>
+#include <bitset>
+#include <set>
+
+using namespace std;
+
+
+
+int main(){
+  
+}
