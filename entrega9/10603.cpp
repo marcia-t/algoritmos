7e4198f31487f78e4c68b0964a0c388451d28e80
Marcia Tejeda <tejedamarcia@gmail.com>
2017-06-15 19:38:32 -0300

    10603

--- entrega9/10603.cpp
+++ entrega9/10603.cpp
@@ -10,9 +10,14 @@
 #include <set>
 
 using namespace std;
-
+int a, b, c, d;
 
 
 int main(){
-  
+  int n;
+  cin >> n;
+  for (size_t i = 0; i < n; i++) {
+    cin >> a >> b >> c >> d;
+    
+  }
 }
