f4c81a9f5ff83c4a679ccd1eed36839538714cc0
marcia-t <tejedamarcia@gmail.com>
2017-06-29 23:50:38 -0300

    1235 start

---
+++ entrega10/1235.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+using pi = pair<int,int>;
+using vpi = vector<pi>;
+using graph = vector<vpi>;
+
+
+
+int main(){
+  
+}
