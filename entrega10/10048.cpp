25962685d0917528c9e97da4c0d6b349adaaa11d
marcia-t <tejedamarcia@gmail.com>
2017-06-29 23:48:00 -0300

    10048 complejidad

--- entrega10/10048.cpp
+++ entrega10/10048.cpp
@@ -5,14 +5,23 @@ using namespace std;
 using pi = pair<int,int>;
 using vpi = vector<pi>;
 using graph = vector<vpi>;
-using minmax = vector<vector<int>>;
+
+/*
+COMPLEJIDAD
+El algoritmo de Prim implementado con colas de prioridad tiene complejidad O(m log n) con:
+n: cantidad de vértices
+m: cantidad de aristas
+La cantidad de vértices estará acotada por  C (peor caso, ya que si encontramos
+el destino antes, finaliza la ejecución), mientras que las aristas estarán acotadas por S (peor caso también).
+Vamos a ejecutar el algoritm de Prim Q veces.
+Quedando entonces la complejidad: O (Q * S log C)
+*/
 
 constexpr int INF = 1000*1000;
 int C, S, Q;
 graph G;
 vector<int> DIST;
 vector<bool> VISITED;
-
 
 int prim(int o, int d){
     int u,w;
@@ -26,7 +35,7 @@ int prim(int o, int d){
         if (!VISITED[u]){
             VISITED[u] = true;
             worst_db = max(worst_db, w);
-            if (u == d) return worst_db;
+            if (u == d) return worst_db; // termino cuando encuentro d
             for (pi v : G[u]) {
                 if (!VISITED[v.second] && DIST[v.second] > v.first){
                     DIST[v.second] = v.first;
@@ -35,9 +44,8 @@ int prim(int o, int d){
             }
         }
     }
-    return 0;
+    return 0; // si nunca encuentro d, retorno 0
 }
-
 
 int main(){
     int c = 0;
@@ -52,7 +60,6 @@ int main(){
                 G[n].push_back(pi(p,o));
                 G[o].push_back(pi(p,n));
             }
-
             cout << "Case #" << c << '\n';
             for (size_t j = 0; j < Q; j++) {
                 DIST.assign(C+1, INF);
@@ -63,7 +70,6 @@ int main(){
                 if (r== 0) cout << "no path" << '\n';
                 else std::cout << r << '\n';
             }
-
         }
         else break;
     }
